0aa9d6713090b2e53303b9d8787c1832c04e9754
Yehia Zakaria <yehiazakaria259@gmail.com>
2023-09-19 22:03:58 +0300

    login

---
+++ selfhosting/selfhosting/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+namespace selfhosting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080"; // Specify the base address for your server
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine($"Server running at {url}");
+                Console.ReadLine();
+            }
+        }
+    }
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll); // Enable cross-origin requests if needed
+            app.MapSignalR(); // Map SignalR hub
+        }
+    }
+
+    public class MyHub : Hub
+    {
+        public void Send(string message)
+        {
+            Clients.All.broadcastMessage(message);
+        }
+    }
+}
